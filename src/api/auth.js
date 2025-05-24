06f01a878d8da25a2c7c0783e140ad5044b2b005
PushpakChaudhari <pushpakchaudhari383@gmail.com>
2025-05-24 08:59:21 +0530

    update

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,15 +2,19 @@ import axios from 'axios';
 
 const API = 'https://pharmacare-backend.onrender.com/api/auth';
 
-export const login = (user) => axios.post(`${API}/login`, user);
-export const register = (user) => axios.post(`${API}/register`, user);
+const axiosInstance = axios.create({
+  baseURL: API,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
-
+export const login = (user) => axiosInstance.post('/login', user);
+export const register = (user) => axiosInstance.post('/register', user);
 
 export const sendResetLink = (email) => {
-    return axios.post(`${API}/forgot-password`, { email });
-  };
+  return axiosInstance.post('/forgot-password', { email });
+};
 
-  export const resetPassword = ({ token, newPassword }) =>
-    axios.post(`${API}/reset-password`, { token, newPassword });
-  
+export const resetPassword = ({ token, newPassword }) =>
+  axiosInstance.post('/reset-password', { token, newPassword });
